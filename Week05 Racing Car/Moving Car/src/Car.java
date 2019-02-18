e0d29bc8574d45c1970b4ac04fdab9e484e28b78
JShull97 <38473222+JShull97@users.noreply.github.com>
2019-02-18 14:41:42 -0600

    Add files via upload

---
+++ Week05 Racing Car/Moving Car/src/Car.java
@@ -0,0 +1,51 @@
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
+
+public class Car extends Pane{
+    Rectangle body = new Rectangle(250, 40);
+    Circle wheel1 = new Circle(30);
+    Circle wheel2 = new Circle(30);
+    Polygon top = new Polygon();
+    
+    public void setCar() {
+        body.setStroke(Color.BLACK);
+        body.setFill(Color.RED);
+        body.setX(20);
+        body.setY(100);
+        wheel1.setStroke(Color.BLACK);
+        wheel1.setFill(Color.BLACK);
+        wheel1.setLayoutX(70);
+        wheel1.setLayoutY(150);
+        wheel2.setStroke(Color.BLACK);
+        wheel2.setFill(Color.BLACK);
+        wheel2.setLayoutX(225);
+        wheel2.setLayoutY(150);
+        
+        top.getPoints().addAll(new Double[]{
+           50.0, 100.0, 
+           100.0, 50.0,
+           150.0, 50.0,
+           200.0, 100.0
+        });
+        top.setStroke(Color.BLACK);
+        top.setFill(Color.BLUE);
+        
+        getChildren().clear();
+        getChildren().addAll(body, wheel1, wheel2,top);   
+    }
+  
+    @Override
+  public void setWidth(double width) {
+    super.setWidth(width);
+    setCar();
+  }
+  
+  @Override
+  public void setHeight(double height) {
+    super.setHeight(height);
+    setCar();
+  }
+}
---
+++ Week05 Racing Car/Moving Car/src/MovingCar.java
@@ -0,0 +1,75 @@
+import javafx.animation.PathTransition;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.shape.Line;
+import javafx.util.Duration;
+
+public class MovingCar extends Application {
+  @Override 
+  public void start(Stage primaryStage) {
+    Car car = new Car();
+    
+    HBox hBox = new HBox();
+    hBox.setSpacing(10);
+    hBox.setAlignment(Pos.CENTER);
+    Button btStart = new Button("Start");
+    Button btStop = new Button("Stop");
+    hBox.getChildren().addAll(btStart, btStop);
+   
+    PathTransition pt = new PathTransition(Duration.millis(10000),
+           new Line(475, 200, 2250, 200), car 
+    );
+    pt.setCycleCount(Timeline.INDEFINITE);
+    
+    EventHandler<ActionEvent> start = new EventHandler<ActionEvent>() {
+        public void handle(ActionEvent e) {  
+            pt.play();
+        }
+    };
+    btStart.setOnAction(start);
+    
+    EventHandler<ActionEvent> stop = new EventHandler<ActionEvent>() {
+        public void handle(ActionEvent e) {
+            pt.pause();
+        }
+    };
+    btStop.setOnAction(stop);
+    
+    EventHandler<KeyEvent> keyListener = new EventHandler<KeyEvent>() {
+        @Override 
+        public void handle(KeyEvent event) {
+            if(event.getCode() == KeyCode.UP) {
+                pt.setRate(pt.getRate() + 0.1);
+            } else if (event.getCode() == KeyCode.DOWN) {
+                pt.setRate(pt.getRate() > 0 ? pt.getRate() - 0.1 : 0);
+            }
+        }
+    };
+      
+    // Place car and buttons in border pane
+    BorderPane pane = new BorderPane();
+    pane.setCenter(car);
+    pane.setBottom(hBox);
+    pane.setOnKeyPressed(keyListener);
+
+    // Create a scene and place it in the stage
+    Scene scene = new Scene(pane, 1500, 300);
+    primaryStage.setTitle("Moving Car"); // Set the stage title
+    primaryStage.setScene(scene); // Place the scene in the stage
+    primaryStage.show(); // Display the stage
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
