77655966d7aa92cf27e3e2e19b1e450271fc982a
JShull97 <38473222+JShull97@users.noreply.github.com>
2019-05-03 16:45:24 -0500

    Update Deck.java

--- FINAL Blackjack/BlackJack/src/Deck.java
+++ FINAL Blackjack/BlackJack/src/Deck.java
@@ -1,20 +1,63 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import javafx.scene.image.Image;
 
 public class Deck {
-    String[] ranks = {"a", "2", "3", "4", "5", "6", "7", "8", "9", "10", "j", "q", "k"};
-    Card[] deck = new Card[52];
-    
-    public Deck() {
-    }
-    
-    public void fillDeck() {
-        for (int i = 1; i <= 4; i++) {
-            for (String rank : ranks) {
-                if (i == 1) Card.setSuit("c");
-                else if (i == 2) Card.setSuit("d");
-                else if (i == 3) Card.setSuit("h");
-                else if (i == 4) Card.setSuit("s");
-                Card.setRank(rank);
+   private List deck;
+   private int index;
+   
+   String[] suits = {"c", "d", "h", "s"};
+   String[] values = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "j", "q", "k"};
+
+   //public Deck(){this(1);}
+   
+   public Deck(int numDecks) {
+        deck = new ArrayList();
+        index = 0;
+       
+        try{
+            for (int suit = 0; suit < 4; suit++) {
+                for (int faceNum = 0; faceNum < 13; faceNum++) {
+                    Card card = new Card(suits[suit], values[faceNum], new Image(Card.getFilename(suits[suit], values[faceNum])));
+                    addCard(card);
+                }
             }
-        }          
-    }
+            shuffle(); 
+        } catch (Exception ex){
+            System.out.println(ex.getMessage()); 
+        }
+   }
+
+   public void addCard(Card card) {
+      deck.add(card);
+   }
+
+   public int getSizeOfDeck() {
+      return deck.size();
+   }
+
+   public int getNumberOfCardsRemaining() {
+      return deck.size() - index;
+   }
+
+   public Card dealCard() {
+      if (index >= deck.size()) return null;
+      else return (Card) deck.get(index++);
+   }
+
+   public void shuffle() {
+      Collections.shuffle(deck);
+   }
+
+   public boolean isEmpty() {
+      if (index >= deck.size())
+         return true;
+      else
+         return false;
+   }
+
+   public void restoreDeck() {
+      index = 0;
+   }   
 }
