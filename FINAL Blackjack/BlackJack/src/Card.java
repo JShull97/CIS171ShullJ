2b5d52d5e012c73d4416d998aeaa223210b0f754
JShull97 <38473222+JShull97@users.noreply.github.com>
2019-05-03 16:45:06 -0500

    Update Card.java

--- FINAL Blackjack/BlackJack/src/Card.java
+++ FINAL Blackjack/BlackJack/src/Card.java
@@ -1,37 +1,43 @@
 import javafx.scene.image.Image;
 
 public class Card {
-    private Image cardImage;
-    private String cardRank;
-    private String cardSuit;
-    
-    public Card(Image cardFace, String rank, String suit) {
-        cardImage = cardFace;
-        cardRank = rank;
-        cardSuit = suit;
-    }
-    
-    public static int getValue(int cardValue) {
-        return 0;
-    }
-    
-    public String getRank() {
-        return this.cardRank;
-    }
-    
-    public String getSuit() {
-        return this.cardSuit;
-    }
-    
-    public static String getImage() {
-        return "Images" + "";
-    }
-    
-    public void setRank(String rank) {
-        this.cardRank = rank;
-    }
-    
-    public void setSuit(String suit) {
-        this.cardSuit = suit;
-    }
+   private final String suitValue;
+   private final String rankValue;
+   private final Image cardImage;
+   
+   String[] suits = {"CLUBS", "DIAMONDS", "HEARTS", "SPADES"};
+   String[] value = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "j", "q", "k"};
+
+   public Card(String suit, String rank, Image cardFace) {
+      cardImage = cardFace;
+      suitValue = suit;
+      rankValue = rank;
+   }
+
+   public static String getFilename(String suit, String rank) {
+      return "Images/"+ suit + rank + ".png";
+   }
+
+   public String getSuit() {
+      return suitValue;
+   }
+
+   public String getRank() {
+      return rankValue;
+   }
+
+   public int getValue() {
+       String rank = this.rankValue;  
+       try{
+           // try to turn it into an integer 
+           return Integer.parseInt(rank);  
+       } catch (Exception ex){
+           // if failed, it's a face card
+               return 10; 
+       }
+   }
+
+   public Image getCardImage() {
+      return cardImage;
+   }
 }
